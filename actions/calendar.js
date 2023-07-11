10ed6daad60ecf9779107d12f4961521b93dbe53
100の人 <100@pokemori.jp>
2023-07-11 16:44:03 +0900

    今日の00:00より前に開始したイベント、および今日の00:00から1週間のあいだに終了しない予定 (日付けをまたぐ予定) も表示するように

--- actions/calendar.js
+++ actions/calendar.js
@@ -8,6 +8,10 @@ const CALENDAR_ID = 'v4hhfbk90aur93dhddd5dmie4g@group.calendar.google.com';
 const DATE_FORMAT
 	= new Intl.DateTimeFormat('ja', { timeZone: TIME_ZONE, month: 'numeric', day: 'numeric', weekday: 'short' });
 const TIME_FORMAT = new Intl.DateTimeFormat('ja', { timeZone: TIME_ZONE, hour: '2-digit', minute: '2-digit' });
+/**
+ * 指定日時をまたいで開始、終了するイベントを取得するために、指定した日付の前後に足す日にち。
+ */
+const MARGIN_DAYS = 30;
 
 /**
  * 本日から1週間以内の予定を取得します。
@@ -21,21 +25,32 @@ export async function fetchWeeklyEventDateTitlesPairs(auth)
 	const events = (await calendar.calendar({ version: 'v3', auth }).events.list({
 		calendarId: CALENDAR_ID,
 		singleEvents: true,
-		timeMin: startOfDay.toISOString(),
-		timeMax: date.addDays(startOfDay, 7).toISOString(),
+		timeMin: date.addDays(startOfDay, - MARGIN_DAYS).toISOString(),
+		timeMax: date.addDays(startOfDay, 7 + MARGIN_DAYS).toISOString(),
 	})).data.items;
 
 	const dateTitlesPairs = new Map();
+	const eventIds = [ ];
 	for (let i = 0; i < 7; i++) {
 		const currentDateTimestamp = date.addDays(startOfDay, i).getTime();
 		const nextDateTimestamp = date.addDays(currentDateTimestamp, 1).getTime();
 
 		const titles = [ ];
 		for (const event of events.filter(function (event) {
-			const timestamp = new Date(event.start.dateTime).getTime();
-			return timestamp >= currentDateTimestamp && timestamp < nextDateTimestamp;
+			if (eventIds.includes(event.id)) {
+				return false;
+			}
+			const startTimestamp = new Date(event.start.dateTime).getTime();
+			const endTimestamp = new Date(event.end.dateTime).getTime();
+			return startTimestamp < currentDateTimestamp
+				? endTimestamp >= currentDateTimestamp // 前日以前に開始したイベント
+				: startTimestamp < nextDateTimestamp;
 		}).sort((a, b) => new Date(a.start.dateTime).getTime() - new Date(b.start.dateTime).getTime())) {
-			titles.push(TIME_FORMAT.format(new Date(event.start.dateTime)) + ' ' + event.summary);
+			titles.push((new Date(event.start.dateTime).getTime() < currentDateTimestamp
+				? '       ↳'
+				: TIME_FORMAT.format(new Date(event.start.dateTime)))
+				+ ' ' + event.summary);
+			eventIds.push(event.id);
 		}
 
 		dateTitlesPairs.set(DATE_FORMAT.format(currentDateTimestamp).replace('/', '月').replace('(', '日('), titles);
